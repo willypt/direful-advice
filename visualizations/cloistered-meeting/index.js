d57cb2434ee6f6d435404f3a8fcebde3e56b98ff
Willy <willy@tokopedia.com>
2022-02-18 03:04:01 +0700

    bug

--- visualizations/cloistered-meeting/index.js
+++ visualizations/cloistered-meeting/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-    Radar,
-    RadarChart,
-    PolarGrid,
-    PolarAngleAxis,
-    PolarRadiusAxis,
-} from 'recharts';
-import {Card, CardBody, HeadingText, NrqlQuery, Spinner, AutoSizer} from 'nr1';
+import {Button, Card, Modal, CardBody, HeadingText, PieChart} from 'nr1';
 
 export default class CloisteredMeetingVisualization extends React.Component {
-    // Custom props you wish to be configurable in the UI must also be defined in
-    // the nr1.json file for the visualization. See docs for more details.
+    constructor(props) {
+        super(props);
+        this.state = {
+            hidden: true,
+        }
+    }
     static propTypes = {
         /**
          * A fill color to override the default fill color. This is an example of
@@ -36,26 +33,6 @@ export default class CloisteredMeetingVisualization extends React.Component {
         ),
     };
 
-    /**
-     * Restructure the data for a non-time-series, facet-based NRQL query into a
-     * form accepted by the Recharts library's RadarChart.
-     * (https://recharts.org/api/RadarChart).
-     */
-    transformData = (rawData) => {
-        return rawData.map((entry) => ({
-            name: entry.metadata.name,
-            // Only grabbing the first data value because this is not time-series data.
-            value: entry.data[0].y,
-        }));
-    };
-
-    /**
-     * Format the given axis tick's numeric value into a string for display.
-     */
-    formatTick = (value) => {
-        return value.toLocaleString();
-    };
-
     render() {
         const {nrqlQueries, stroke, fill} = this.props;
 
@@ -70,48 +47,14 @@ export default class CloisteredMeetingVisualization extends React.Component {
         }
 
         return (
-            <AutoSizer>
-                {({width, height}) => (
-                    <NrqlQuery
-                        query={nrqlQueries[0].query}
-                        accountId={parseInt(nrqlQueries[0].accountId)}
-                        pollInterval={NrqlQuery.AUTO_POLL_INTERVAL}
-                    >
-                        {({data, loading, error}) => {
-                            if (loading) {
-                                return <Spinner />;
-                            }
-
-                            if (error) {
-                                return <ErrorState />;
-                            }
-
-                            const transformedData = this.transformData(data);
-
-                            return (
-                                <RadarChart
-                                    width={width}
-                                    height={height}
-                                    data={transformedData}
-                                >
-                                    <PolarGrid />
-                                    <PolarAngleAxis dataKey="name" />
-                                    <PolarRadiusAxis
-                                        tickFormatter={this.formatTick}
-                                    />
-                                    <Radar
-                                        dataKey="value"
-                                        stroke={stroke || '#51C9B7'}
-                                        fill={fill || '#51C9B7'}
-                                        fillOpacity={0.6}
-                                    />
-                                </RadarChart>
-                            );
-                        }}
-                    </NrqlQuery>
-                )}
-            </AutoSizer>
-        );
+          <>
+              <Button onClick={() => this.setState({hidden: false})}>Open Modal</Button>
+              <Modal hidden={this.state.hidden} onClose={() => this.setState({hidden: true})}>
+                  <PieChart accountIds={[parseInt(nrqlQueries[0].accountId)]} query={nrqlQueries[0].query} fullWidth />
+              </Modal>
+              <PieChart accountIds={[parseInt(nrqlQueries[0].accountId)]} query={nrqlQueries[0].query} fullWidth />
+          </>
+          );
     }
 }
 
@@ -133,20 +76,6 @@ const EmptyState = () => (
             <code>
                 FROM NrUsage SELECT sum(usage) FACET metric SINCE 1 week ago
             </code>
-        </CardBody>
-    </Card>
-);
-
-const ErrorState = () => (
-    <Card className="ErrorState">
-        <CardBody className="ErrorState-cardBody">
-            <HeadingText
-                className="ErrorState-headingText"
-                spacingType={[HeadingText.SPACING_TYPE.LARGE]}
-                type={HeadingText.TYPE.HEADING_3}
-            >
-                Oops! Something went wrong.
-            </HeadingText>
         </CardBody>
     </Card>
 );
